8e892ffd4ed86d941d4f4e105dfb4c1adf2e8a2f
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-15 14:31:15 +0100

    basic click event is responsive

---
+++ TicTacToe/javascript/main.js
@@ -0,0 +1,15 @@
+// var grid = document.getElementsByClassName("square");
+
+// grid.forEach(square, index, array) {
+//   square.addEventListener("click", function(){
+//     this.set
+//   })
+// }
+
+var square1 = document.getElementById("1-1");
+
+square1.addEventListener("click", function(){
+  document.getElementById("1-1-content").className = "x";
+})
+
+console.log("hello");
