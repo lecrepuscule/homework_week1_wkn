04fcf11e6e213bb808f01fe7c76fbe8ab1778a72
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-15 18:15:49 +0100

    added board array

--- TicTacToe/javascript/main.js
+++ TicTacToe/javascript/main.js
@@ -1,17 +1,28 @@
+
+//init game section//
 var player1 = "x";
 var player2 = "o";
 var currentPlayer = player1;
 var nextPlayer;
+var board=[[]];
 
 var grid = document.getElementsByClassName("square");
 
 for (i=0; i<grid.length; i++) {
   grid[i].addEventListener("click", function(){
-    console.log(this);
-    this.getElementsByClassName("empty")[0].className = markSquare();
-    console.log('ouch');
+    this.getElementsByClassName("empty")[0].className = markSquare(this.id.split("-"));
   })
 }
+
+function setupBoard(maxRow, maxColumn){
+  for (r = 0; r < maxRow; r++) {
+    for (c = 0; c < maxColumn; c++) {
+      board[r][c] = "";
+    }
+  }
+}
+//init game section//
+
 
 function playGame() {
   setupGame();
@@ -19,11 +30,17 @@ function playGame() {
   endGame();
 }
 
-function markSquare () {
+function markSquare (position) {
   currentPlayer = nextPlayer || currentPlayer;
   nextPlayer = nextPlayer === player2 ? player1 : player2;
+  var r = parseInt(position[0]);
+  var c = parseInt(position[0]);
+  console.log(currentPlayer);
+  board[r][c] = currentPlayer;
+  console.log(board);
   return currentPlayer;
 }
+
 
 // function setupPlayers() {
 //   player1
