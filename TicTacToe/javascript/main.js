2872d6c1a4e893f1acfc2f50596446ac8cfa43cc
Yao Yuan <opal_obsidian@hotmail.com>
2015-08-15 14:55:37 +0100

    added listener to whole grid

--- TicTacToe/javascript/main.js
+++ TicTacToe/javascript/main.js
@@ -1,15 +1,17 @@
-// var grid = document.getElementsByClassName("square");
+var grid = document.getElementsByClassName("square");
 
-// grid.forEach(square, index, array) {
-//   square.addEventListener("click", function(){
-//     this.set
-//   })
-// }
+for (i=0; i<grid.length; i++) {
+  grid[i].addEventListener("click", function(){
+    console.log(this);
+    this.getElementsByClassName("empty")[0].className = "x";
+    console.log('ouch');
+  })
+}
 
 var square1 = document.getElementById("1-1");
 
-square1.addEventListener("click", function(){
-  document.getElementById("1-1-content").className = "x";
-})
+// square1.addEventListener("click", function(){
+//   document.getElementById("1-1-content").className = "x";
+// })
 
-console.log("hello");
+
